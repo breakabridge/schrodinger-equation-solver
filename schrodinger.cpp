81dac09b3c2c6ce9d5917e919512e36911aa78d4
Arran <93921912+breakabridge@users.noreply.github.com>
2022-01-05 15:17:17 +0000

    Added comments.

--- schrodinger.cpp
+++ schrodinger.cpp
@@ -1,3 +1,9 @@
+/*
+FTCS-method solver of the Schrodinger equation on a 2D grid. Initial condition
+is a Gaussian wavepacket and the scalar potential is a hard wall on all 4 sides (a
+large Gaussian spike).
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -9,15 +15,18 @@ typedef struct complex  {
     float im;
 } complex;
 
+// SIMULATION PARAMETERS. CHANGE AS YOU WISH
 const int N = 99;
 const int size = (N+2) * (N+2);
 float dx = 0.01;
 float dt = 2.5e-6;
 int frames = 400;
 
+// WAVEFUNCTION AND POTENTIAL
 complex psi[size];
 float potential[size];
 
+// COMPLEX FUNCTIONS
 float modSquared(complex z) {
     return z.real * z.real + z.im * z.im;
 }
@@ -61,6 +70,7 @@ float scalarProduct2D(float * x, float * y) {
     return x[0] * y[0] + x[1] * y[1];
 }
 
+// ADDS YOUNG DOUBLE SLITS / HARD WALL. PROGRAMMED AS GAUSSIAN SPIKES WITH SHORT WIDTH
 void addHorizontalSlits(float * potential, int slitWidth, int slitDistance, int y) {
     float constant = 0.5 / (dx * dx);
     int middle = (N+1) / 2;
@@ -74,6 +84,7 @@ void addHorizontalSlits(float * potential, int slitWidth, int slitDistance, int 
     }
 }
 
+// SETS EVERYTHING TO 0. MUST BE INCLUDED IN MAIN.
 void initialise(complex * psi, float * potential)   {
     complex zero; zero.real = 0; zero.im = 0;
     for (int i = 0; i < N+2; i++)   {
@@ -84,6 +95,7 @@ void initialise(complex * psi, float * potential)   {
     }
 }
 
+// INSERTS GAUSSIAN WAVEPACKET AT POSITION r0 WITH WAVEVECTOR k AND WIDTH sigma. 
 void setup(complex * psi, float * potential, float * r0, float * k, float sigma) {
     float constant1 = 0.5 / (sigma * sigma);
     float constant2 = 0.5 / (dx * dx);
@@ -100,7 +112,7 @@ void setup(complex * psi, float * potential, float * r0, float * k, float sigma)
             }
         }
 
-    // The following code may not be needed. This is to normalise the wavefunction so that the integral over the grid is 1.
+    // THE FOLLOWING CODE MAY NOT BE NEEDED. NORMALISES WAVEFUNCTION SO INTEGRAL OVER GRID OF |psi|^2 = 1.
     float integral = 0;
     for (int i = 0; i < N+2; i++)  {
         for (int j = 0; j < N+2; j++)   {
@@ -115,12 +127,14 @@ void setup(complex * psi, float * potential, float * r0, float * k, float sigma)
     }
 }
 
+// RETURNS i * [laplacian(psi) - potential * psi]. FOR USE IN RUNGE-KUTTA STEP.
 complex function(complex * psi, complex RKConstant, float * potential, int i, int j)  {
     complex constant; constant.real = 0; constant.im = 0.5 / (dx * dx);
 
     return constant * (psi[IX(i+1, j)] + psi[IX(i-1, j)] + psi[IX(i, j+1)] + psi[IX(i, j-1)] - 4 * (psi[IX(i, j)] + RKConstant)) - potential[IX(i, j)] * (psi[IX(i, j)] + RKConstant);
 }
 
+// THE GOOD OLD EULER FORWARD STEP THAT DID NOT WORK VERY WELL. SHOULD PROBABLY BE REMOVED.
 void eulerStep(complex * psi)   {
     complex zero; zero.real = 0; zero.im = 0;
     complex * store = psi;
@@ -131,10 +145,11 @@ void eulerStep(complex * psi)   {
     }
 }
 
+// THE MUCH BETTER RUNGA-KUTTA 4th ORDER STEP.
 void RK4Step(complex * psi) {
     complex k1, k2, k3, k4, zero;
-    zero.real = 0; zero.im = 0;
-    complex * store = psi;
+    zero.real = 0; zero.im = 0; // 0 represented as 0 + 0i
+    complex * store = psi; // temporary storage of psi
     for (int i = 1; i < N+1; i++)   {
         for (int j = 1; j < N+1; j++)   {
             k1 = function(psi, zero, potential, i, j);
@@ -153,20 +168,21 @@ int main()  {
 
     initialise(psi, potential);
     setup(psi, potential, r0, k, sigma);
-    addHorizontalSlits(potential, 0, 10, (N+1)/2);
+    addHorizontalSlits(potential, 0, 10, (N+1)/2);  // CAN BE DELETED, CHANGED, ETC.
+    
     std::ofstream file;
-    file.open("double_slit_sim.dat");
-    srand(time(NULL));
+    file.open("schrodinger_sim.dat");
     file << N << "," << frames << "," << "\n";
 
     for (int i = 0; i < frames; i++)    {
         std::cout << i << "\n";
-        for (int k = 0; k < 16; k++)    {
+        for (int k = 0; k < 16; k++)    {   // DONE 16 TIMES EVERY FRAME SO THE VIDEO ISN'T TOO SLOW
             RK4Step(psi);
         }
+        
         for (int x = 0; x < N+2; x++)   {
             for (int y = 0; y < N+2; y++)   {
-                file << modSquared(psi[IX(x, y)]) << ",";
+                file << modSquared(psi[IX(x, y)]) << ",";   // WE OUTPUT |psi|^2 TO THE FILE
             }
         }
         file << "\n";
