0eff8d89131117149600672fc7806f9811392a23
Arran <93921912+breakabridge@users.noreply.github.com>
2022-01-06 19:57:13 +0000

    Update schrodinger.cpp

--- schrodinger.cpp
+++ schrodinger.cpp
@@ -180,8 +180,8 @@ int main()  {
             RK4Step(psi);
         }
         
-        for (int x = 0; x < N+2; x++)   {
-            for (int y = 0; y < N+2; y++)   {
+        for (int x = 1; x < N+1; x++)   {
+            for (int y = 1; y < N+1; y++)   {
                 file << modSquared(psi[IX(x, y)]) << ",";   // OUTPUTS |psi|^2 TO THE FILE.
             }
         }
